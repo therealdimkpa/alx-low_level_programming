be61363fa98696ad519d697b558ff6b9e4e68468
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-13 19:35:22 +0100

    Prints the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints the alphabet 10x
+*
+* Return: Returns nothing
+*/
+
+void print_alphabet_x10(void)
+{
+char item;
+int loop = 0;
+
+while (loop < 10)
+	{
+	for (item = 'a'; item < 'z'; item++)
+		_putchar(item);
+	if (loop != 9)
+		_putchar('\n');
+	}
+}
