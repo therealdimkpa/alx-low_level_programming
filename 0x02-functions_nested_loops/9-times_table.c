5dd2eb0c99b1f14fd3ce8fc3f5ea3c91a67fa00e
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-13 23:56:30 +0100

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@
 void times_table(void)
 {
 int start = 0;
+
 int stop = 0;
 
 for (start = 0; start <= 9; start++)
@@ -14,7 +15,11 @@ for (start = 0; start <= 9; start++)
 	for (stop = 0; stop <= 9; stop++)
 		{
 		int result = stop * start;
-		printf("%i,..", result);
+
+		if (start * (stop+1) < 10)
+			printf("%i, ", result);
+		else
+			printf("%i, ", result);
 		}
 	printf("\n");
 	}
