6add4916222563343b222b2216fe776f66d9c2b1
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-14 08:51:06 +0100

    Fib values less than 4,000,000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+* main - entry
+* 
+*Return: 0
+*
+*/
+
+int main(void)
+{
+/* the first member of the fibonacci sequence */
+long int val1 = 1;
+
+/* the second member of the fibonacci sequence */
+long int val2 = 2;
+
+/* stores the new most recent fibonacci member */
+int lastvalue = 0;
+
+/* the maximum permissible fibonacci value */
+int max_value = 4000000;
+
+/* stors the sum of all even fibonacci numbers */
+int sum_of_even = 2;
+
+/*printf("inital values: %li, %li\n", val1, val2);
+*printf("initial value of sum of evens: %i\n", sum_of_even);
+*/
+
+while (val2 < max_value)
+        {
+        lastvalue = val1 + val2;
+        val1 = val2;
+        val2 = lastvalue;
+	
+	if ((val2 % 2 == 0) && (val2 < max_value))
+		sum_of_even += val2;
+        }
+
+printf("%i\n", sum_of_even);
+
+return (0);
+}
