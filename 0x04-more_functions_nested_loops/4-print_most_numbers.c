179e0ad7e51fa2533d00388713ce53de1f6350ae
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-15 12:44:33 +0100

    Print all numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_most_numbers	-	Entry
+*/
+
+void print_most_numbers(void)
+	{
+	int item;
+
+	for (item = 0; item <= 9; item++)
+		{
+		if (item == 2 || item == 4)
+			continue;
+		else
+			printf("%i", item);
+		}
+	printf("\n");
+	}
