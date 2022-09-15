c188644b858b60e239a4c3fb37185b12a334597f
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-15 08:03:00 +0100

    Eliminates infinite loop

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,8 +8,11 @@
 
 int main(void)
 {
-	int i = 98;
+	int i;
+
+	i = 98;
 
 	positive_or_negative(i);
+
 	return (0);
 }
---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* positve_or_negative - entry point
+* @i: integer to be testes
+*/
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}
