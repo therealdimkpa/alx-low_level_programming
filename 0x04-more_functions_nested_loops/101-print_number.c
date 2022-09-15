f47bcf6c0a0fb9c14f6dff4f11806da6738d2892
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-15 21:50:53 +0100

    Print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_number - Prints an integer.
+*
+* @n: The integer to be printed.
+*/
+
+void print_number(int n)
+{
+unsigned int num = n;
+_putchar(num + '0');
+_putchar('\n');
+}
