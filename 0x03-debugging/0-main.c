6ea9c7b4fb067f6137889b991bdbe93f99f91aa8
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-15 07:47:53 +0100

    Debugs positivity or negativity function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - tests function that checks the sign of an integer
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i = 98;
+
+	positive_or_negative(i);
+	return (0);
+}
