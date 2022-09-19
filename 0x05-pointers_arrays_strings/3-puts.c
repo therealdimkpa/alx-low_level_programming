e394aa1c06f116b58a345c2045f0df867192b79b
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-19 23:40:32 +0100

    Puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,26 @@
 #include "main.h"
 
 /**
+* _strlen -	Gets the length of a string
+* @s:		string
+* Return:	length of string
+*
 * _puts -	Writes a string to stdout
 * @str:		The string to be written
 * Return:	Nothing
 */
+
+int _strlen(char *s)
+{
+int length = 0;
+
+while (s[length] != '\0')
+    {
+    length++;
+    }
+
+return (length);
+}
 
 void _puts(char *str)
 {
