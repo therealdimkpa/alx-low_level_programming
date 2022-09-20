e4950815ca9f2d4d3fe27b881fc604d3ef48ffa4
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-20 01:22:25 +0100

    Prints an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_array -	prints a specific number of members of an array
+* @a:			Parent array
+* @n:			Number of members to print
+* Return:		Nothing
+*/
+
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+	{
+	if (i == (n-1))
+		printf("%i\n", a[i]);
+	else
+		printf("%i, ", a[i]);
+	}
+}
