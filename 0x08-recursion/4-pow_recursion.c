7df3255867af25fb0ba55c4076097e2a8cb5e261
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-28 19:25:16 +0100

    Betty compliance for 3-factorial and Power function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _pow_recursion -	Multiplies a number a certain number of times
+* @x:				Number to be multiplied
+* @y:				Number of times x should be multiplied
+* Return:			Returns x raised to the power of y or -1 if y is negative
+*/
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+	return (-1);
+
+else if (y == 1)
+	return (x);
+
+else
+	return (x * _pow_recursion(x, y--));
+}
