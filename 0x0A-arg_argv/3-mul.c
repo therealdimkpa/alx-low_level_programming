213d855eba3e1e47846b3ea5c0b7c32662720494
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-30 12:12:02 +0100

    Multiplication

---
+++ 0x0A-arg_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* main -	Multiplies two numbers
+* @argc:	Number of arguments passed to the program
+* @argv:	Array of pointers of arguments passed to the program
+* Return:	0 if successful else, 1
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		{
+		printf("Error\n");
+		return (1);
+		}
+
+	else
+		{
+
+		int i;
+
+		int a = atol(argv[1]);
+		int b = atol(argv[2]);
+
+		int c = a * b;
+
+		printf("%i\n", c);
+		return (0);
+		}
+}
