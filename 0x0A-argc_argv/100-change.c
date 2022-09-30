ee0683725660e96ca8d3e7c236dd0b2ece941345
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-09-30 16:32:16 +0100

    Renamed Repository

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
+else
+	{
+	int coins[] = {1, 2, 5, 10, 25};
+	int no_of_coints = 5;
+	int i;
+	int value = atoi(argv[1]);
+
+	
+	}
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+Argc and Argv
+
+0. Prints the name of the program
+1. Prints the number of arguments passed to the program
+2. Prints out all arguments passed to the program
