e3d1f2f1928703b1823d531103f19a808461953b
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-04 21:39:30 +0100

    Creates array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 
 /**
-* create_array - 	Creates an initializes an array of specific
-* @size:			This is the size of the array
+* create_array -	Creates an initializes an array of specific
+* @size:				This is the size of the array
 * @c:				This is the char it should be filled with
-*
 * Return:			Null if size of arrat is 0
 *					Pointer to the first element of the array
 */
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    char *a;
+	unsigned int i;
+	char *a;
 
 	if (size == 0)
 		return (NULL);
