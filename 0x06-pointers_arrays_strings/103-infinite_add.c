b68356bc6e7f934b2e52c545f52133a8babe977f
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-09 16:17:25 +0100

    Infinite add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*infinite_add - function
+*@n1:char parameter
+*@n2:char parameter
+*@r :int parameter
+*@size_r : int parameter
+*Return: char value of added sum
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int len1 = strlen(n1);
+int len2 = strlen(n2);
+int max =  len1 > len2 ? len1 : len2;
+if ((max + 1) >= (size_r))
+	return (0);
+else
+	{
+	int sum, num1, num2;
+
+	num1 = atoi(n1);
+	num2 = atoi(n2);
+	sum = num1 + num2;
+	printf("%i", sum);
+	*r = sum + '0';
+
+	return (r);
+	}
+}
