d6f502f5dfd47d0ab6b24eb2eb5666b2133f720f
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-12 21:09:13 +0100

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+* print_name - Prints a name
+* @name:		The name to be printed
+* @f:			Pointer to a func
+* Return: Null Void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
+
