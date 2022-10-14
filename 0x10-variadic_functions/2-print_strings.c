e11d17497f243aff26536008e1b9eefc9eff9bf8
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-15 00:37:50 +0100

    Print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,8 +13,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *store;
 
-	int *arr = malloc(sizeof(int) * n);
-
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
