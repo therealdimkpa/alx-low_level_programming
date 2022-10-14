ecdec8658aabbf1e2535d57397a8a78c7e9aff73
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-14 23:58:58 +0100

    Sum them all

--- 0x10-variadic-functions/0-sum_them_all.c
+++ 0x10-variadic-functions/0-sum_them_all.c
@@ -1,8 +1,10 @@
 #include "variadic_functions.h"
 
 /**
-*
-*
+* sum_them_all -	Sums all numbers passed to the function
+* @n:				Number of arguments to be passed
+* Return:			0 if n = 0
+*					Sum of the first n arguments
 */
 
 int sum_them_all(const unsigned int n, ...)
