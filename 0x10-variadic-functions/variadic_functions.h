8a9690361240ab6c4ce082bb96fdf555a26e3457
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-15 00:19:06 +0100

    Print all

--- 0x10-variadic-functions/variadic_functions.h
+++ 0x10-variadic-functions/variadic_functions.h
@@ -4,8 +4,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* V_FUNC */
