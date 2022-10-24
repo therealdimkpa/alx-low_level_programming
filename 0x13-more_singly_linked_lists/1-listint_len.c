e86792abcfa4e8eba8a257053a82dc2a6d6a622b
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-24 20:38:50 +0100

    Prints list len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* list_len -	Prints the number of nodes in a list
+* @h:			The head/start of the list
+* Return:		THe size of the list
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+		{
+		size++;
+		h = h->next;
+		}
+
+	return (size);
+}
