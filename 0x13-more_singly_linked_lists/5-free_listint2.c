519a4d330b5db181c5596e09d34b21d2016100b4
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-25 21:40:14 +0100

    Free my lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* free_listint2 -	Frees the memory held by a list;
+* @head:			First element of the list
+* Return:			Returns nothing
+*/
+
+void free_listint2(listint_t *head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	else
+	{
+		while (head)
+		{
+			tmp = head->next;
+			free(head);
+			head = tmp;
+		}
+		head = NULL;
+	}
+}
