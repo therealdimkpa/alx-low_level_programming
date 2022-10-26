499ea7e6d345327fee7732f0ad526e094c83ca8f
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-26 10:56:09 +0100

    Reverse list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,24 +11,23 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *prev, *curr;
 
 	if (*head != NULL)
-		{
-			prev = *head;
-			*head = (*head)->next;
-			prev->next = NULL;
-			curr = *head;
-		}
+	{
+		prev = *head;
+		*head = (*head)->next;
+		prev->next = NULL;
+		curr = *head;
+	}
 	else
-		{
-			return (NULL);
-		}
+		return (NULL);
+
 	while (*head != NULL)
-		{
-			*head = (*head)->next;
-			curr->next = prev;
-			prev = curr;
-			curr = *head;
-		}
+	{
+		*head = (*head)->next;
+		curr->next = prev;
+		prev = curr;
+		curr = *head;
+	}
 	*head = prev;
 
-	return (*head);	
+	return (*head);
 }
