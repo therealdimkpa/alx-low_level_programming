80374a0940feac96d475cfc323149e7d220e1107
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-27 15:47:58 +0100

    Prints in binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char n);
 
 #endif /* MAIN_H */
