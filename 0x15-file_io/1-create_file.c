7742ab58f84c76e317b7c10a1adb35392db95dcc
Emmanuel Dimkpa <dimkpaemmanuel1@gmail.com>
2022-10-31 23:32:41 +0100

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
 * @filename:		A pointer to the name of the file to create.
 * @text_content:	A pointer to a string to write to the file.
 * Return:			If the function fails - -1.
-*         			Otherwise - 1.
+*					Otherwise - 1.
 */
 
 int create_file(const char *filename, char *text_content)
