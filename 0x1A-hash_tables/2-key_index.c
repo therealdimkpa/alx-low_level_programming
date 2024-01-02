7061d8c36490949a7399ef3844460a7162fb65d8
Dimkpa Emmanuel <dimkpaemmanuel1@gmail.com>
2024-01-02 08:46:12 +0100

    Betty fix?

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/*
+* key_index: Generates index for any string using the djb2 algorithm
+* @key: The string to be hashed
+* @size: The size of the hash array the index is to be used
+* Return: The index to be used in the hash table
+*/
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size){
     unsigned long int hash;
     unsigned long int index;
