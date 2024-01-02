822370c00773059a71531e17ed159af952222c47
Dimkpa Emmanuel <dimkpaemmanuel1@gmail.com>
2024-01-02 08:30:03 +0100

    Fixed betty, added hashing algorithm

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hashtable
- * @size -  The size of the hastable to be created
- * Return-  Pointer to the hashtable
+ * hash_table_create: Creates a hashtable
+ * @size:  The size of the hastable to be created
+ * Return: -  Pointer to the hashtable
 */
 
 hash_table_t *hash_table_create(unsigned long int size){
